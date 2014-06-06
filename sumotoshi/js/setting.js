7613c953d1cb77c8c000f81f5cdd97c38263a1bc
yoshinobu <milk@milk-2.local>
2014-06-06 09:16:39 +0900

    洲本版　追加

---
+++ sumotoshi/js/setting.js
@@ -0,0 +1,85 @@
+/*
+ * Icons for COFOG taxonomy
+ *
+ */
+var Taxes = Taxes || {};
+
+Taxes.baseKoujo = 330000; // 住民税基礎控除
+Taxes.huyoKoujo = 330000; // 一人分の扶養控除
+Taxes.taxRate = 0.06; // 住民税率
+
+var OpenSpending = OpenSpending || {};
+
+OpenSpending.identifier = 'sumoto_2012_saisyutsumikomi';
+OpenSpending.year = '2012';
+
+OpenSpending.Styles = OpenSpending.Styles || {};
+
+OpenSpending.Styles.Cofog = {
+
+  /* minamiawajicofog */
+  //議会費
+  '20120100': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#935B3B' }, 
+  '20120101': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#935B3B' }, 
+  
+   //総務費
+  '20120200': { icon: 'icons/financial-admin.svg', color: '#C75746', bcolor: '#0AB971' }, 
+  '20120201': { icon: 'icons/financial-admin.svg', color: '#C75746', bcolor: '#0AB971' }, 
+
+
+  //民生費
+  '20120300': { icon: 'icons/family.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '20120301': { icon: 'icons/family.svg', color: '#C75746', bcolor: '#4E6D00' },
+
+  //衛生費
+
+  '20120400': { icon: 'icons/health.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '20120401': { icon: 'icons/health.svg', color: '#C75746', bcolor: '#4E6D00' },
+
+  //労働費
+
+  '20120500': { icon: 'icons/misc-services.svg', color: '#C75746', bcolor: '#D33673' },
+  '20120501': { icon: 'icons/misc-services.svg', color: '#C75746', bcolor: '#D33673' },
+
+  //農林水産業費
+
+  '20120600': { icon: 'icons/c_nougyou.svg', color: '#C75746', bcolor: '#2A3A03' },
+  '20120601': { icon: 'icons/c_nougyou.svg', color: '#C75746', bcolor: '#2A3A03' },
+
+  //商工費
+
+  '20120700': { icon: 'icons/c_syoukougyou.svg', color: '#C75746', bcolor: '#2A3A03'  },
+  '20120701': { icon: 'icons/c_syoukougyou.svg', color: '#C75746', bcolor: '#2A3A03'  },
+  
+  //土木費
+  '20120800': { icon: 'icons/snow-removal.svg', color: '#C75746', bcolor: '#938626' },
+  '20120801': { icon: 'icons/snow-removal.svg', color: '#C75746', bcolor: '#938626' },
+
+  // 消防費
+
+  '20120900': { icon: 'icons/fire_engine.svg', color: '#C75746', bcolor: '#C75746' },//
+  '20120901': { icon: 'icons/fire_engine.svg', color: '#C75746', bcolor: '#C75746' },//
+
+  //教育費
+
+  '20121000': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#790586' },
+  '20121001': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#790586' },//小・中学校運営に
+
+ //災害復旧費
+ 
+ '20121100': { icon: 'icons/euro.svg', color: '#C75746', bcolor: '#790586' },//
+ '20121101': { icon: 'icons/euro.svg', color: '#C75746', bcolor: '#790586' },//農施設の復旧等に
+
+ //公債費
+ '20121200': { icon: 'icons/transfers.svg', color: '#C75746', bcolor: '#790586' },//
+ '20121201': { icon: 'icons/transfers.svg', color: '#C75746', bcolor: '#790586' },//返済に
+
+ //諸支出金
+ '20121300': { icon: 'icons/money.svg', color: '#C75746', bcolor: '#790586' },//
+ '20121301': { icon: 'icons/money.svg', color: '#C75746', bcolor: '#790586' },//諸支出金に
+ //予備費
+ '20121400': { icon: 'icons/pig.svg', color: '#C75746', bcolor: '#790586' },//予備費
+ '20121401': { icon: 'icons/pig.svg', color: '#C75746', bcolor: '#790586' },//
+};
+
+
